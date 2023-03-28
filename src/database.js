af5dd1cbd947cf43bc287cc95e7794e1ff859834
Leonardo Bonfim <leo.bonfim.95@gmail.com>
2023-03-27 21:10:33 -0300

    Commit initial

---
+++ src/database.js
@@ -0,0 +1,59 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
+export class Database { 
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf-8').then(data => {
+      this.#database = JSON.parse(data);
+    }).catch(()=> {
+      this.#persist
+    });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    let data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if (rowIndex !== -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    const row = this.#database[table].find(row => row.id === id);
+
+    const taskUpdate = {
+      ...row,
+      ...data
+    }
+
+    if (rowIndex !== -1) {
+      this.#database[table][rowIndex] = { id, ...taskUpdate };
+      this.#persist();
+    }
+  }
+}
